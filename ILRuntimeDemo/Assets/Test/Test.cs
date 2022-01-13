f8142e3cf0cc9fa3c6eac66c1717db21d8effa18
Hellsing <luozichu@bilibili.com>
2022-01-13 18:03:20 +0800

    [feat]新增性能图形显示

---
+++ ILRuntimeDemo/Assets/Test/Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public GameObject cube;
+    private void Awake()
+    {
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                var instance = Instantiate(cube,this.transform);
+                //Destroy(instance);
+            }
+
+            stopwatch.Stop();
+            print($"Milliseconds: {stopwatch.ElapsedMilliseconds}");
+        }
+    }
+}
